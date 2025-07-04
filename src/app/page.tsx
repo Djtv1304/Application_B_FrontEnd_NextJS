04579d8f68be9e40a27ce1a5986fb5dcc8c6a93e
Diego <dtoscano@metropolitan-touring.com>
2025-07-04 13:49:15 -0500

    NextJS configuration and logic added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
 import Image from "next/image";
+import Link from "next/link";
+import {getSignInUrl, getSignUpUrl, signOut, withAuth} from "@workos-inc/authkit-nextjs";
 
-export default function Home() {
+export default async function Home() {
+
+  const { user } = await withAuth();
+  const signUpUrl = await getSignUpUrl();
+  const signInUrl = await getSignInUrl();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <header className='flex gap-4'>
+          { user ? (
+              <>
+                <form
+                    action={async () => {
+                        'use server';
+                        await signOut({
+                          returnTo: 'http://localhost:3000/',
+                        });
+                    }}
+                >
+                  <p>Welcome, {user.firstName}</p>
+                  <button type='submit'>Sign out</button>
+                </form>
+              </>
+          ): (
+              <>
+                <Link href={signUpUrl}>Sign up</Link>
+                <Link href={signInUrl}>Sign in</Link>
+              </>
+        )}
+
+        </header>
         <Image
           className="dark:invert"
           src="/next.svg"
